8d1b24292b542011f2ee482e29e44d5bf0d722c5
Steve Motsco <stephenmotsco@g.harvard.edu>
2014-10-12 20:10:06 -0500

    Updates after adding 'paste/pre' package to foobooks.

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,52 @@
 |
 */
 
+# Homepage
 Route::get('/', function()
 {
-	return View::make('hello');
+	return View::make ("index.php");
+	//return View::make('hello');
 });
+
+
+Route::get('/books', function() {
+
+	return "Here are all the books...";
+
+});
+
+Route::get('/books/{category}', function($category) {
+
+	return "Here are the books in the category: ".$category;
+
+});
+
+
+Route::get('/new', function() {
+
+    $view  = '<form method="POST" action="/new">';
+    $view .= 'Title: <input type="text" name="title">';
+    $view .= '<input type="submit">';
+    $view .= '</form>';
+    return $view;
+
+});
+
+Route::post('/new', function() {
+
+    $input =  Input::all();
+    print_r($input);
+
+});
+
+
+Route::get('/data', function() {
+
+    //get the file, convert to array, return the file as an array
+    $books = File::get(app()_path.'/databse/books.json');
+    $books = json_decode($books, true);
+    return $books;
+
+});
+
+
---
+++ index.blade.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv=""X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="TEMPLATE">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Steve Motsco">
+    
+    <link rel="stylesheet" href="css/,DanaInfo=webmail2010.goldlnk.rootlnka.net,SSL+style.css" type=""text/css">
+    <style type="text/css"></styles>
+    <title>TEMPLATE</title>
+    <!--[if lt IE 9]
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    [endif]-->
+</head>
+
+<body>
+<script src="js/scripts.js"></script>
+</body>
+<script type="text/javascript"></script>
+</html>
